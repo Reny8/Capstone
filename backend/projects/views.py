62e3921e463ce6e128147c7d91018c04c4c646e2
Reny Reyes <renyreyes8@hotmail.com>
2022-05-19 15:41:26 -0400

    Modified get_all_projects function to only display current user projects

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework import status
@@ -5,10 +6,21 @@ from .models import Projects
 from .serializers import ProjectSerializer
 from rest_framework.permissions import IsAuthenticated
 
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_all_projects(request):
     if request.method == 'GET':
-        projects = Projects.objects.all()
+        projects = Projects.objects.filter(owner_id = request.user.id)
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def project_details(request,pk):
+    project = get_object_or_404(Projects, pk = pk)
+    if request.method == 'GET':
+        pass
+        
+
