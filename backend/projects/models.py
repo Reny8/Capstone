7440c0443415b7cd28546efaa632b2e5f6bab707
Reny Reyes <renyreyes8@hotmail.com>
2022-05-19 14:19:34 -0400

    Created a project manager user + Team member users

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,9 +1,8 @@
-from io import open_code
 from django.db import models
 from authentication.models import User
 # Create your models here.
 class Projects(models.Model):
-    owner = models.ForeignKey(User, related_name="admin", on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name="owner", on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     due_date = models.DateField()
-    assigned_users = models.ManyToManyField(User)
+    assigned_users = models.ManyToManyField(User, related_name="assigned_users")
