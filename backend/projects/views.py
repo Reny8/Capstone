e30fb76a6643cd17eeb3f3fcf712208b70ce7671
Reny Reyes <renyreyes8@hotmail.com>
2022-05-19 15:25:45 -0400

    Created the functionality to get all projects

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Projects
+from .serializers import ProjectSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_all_projects(request):
+    if request.method == 'GET':
+        projects = Projects.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
