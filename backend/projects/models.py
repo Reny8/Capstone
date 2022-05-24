a0bb0cf53031aaa7e0b00017f08d84248c5f4b17
Reny Reyes <renyreyes8@hotmail.com>
2022-05-23 20:16:57 -0400

    Adjusted the datefield for the logs

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -4,5 +4,5 @@ from authentication.models import User
 class Projects(models.Model):
     owner = models.ForeignKey(User, related_name="owner", on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    due_date = models.DateField()
+    due_date = models.CharField(max_length=50)
     assigned_users = models.ManyToManyField(User)
