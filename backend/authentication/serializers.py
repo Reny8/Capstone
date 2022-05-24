f9e0e8cdc70dad57a895b31509adc43e0aba51f6
Reny Reyes <renyreyes8@hotmail.com>
2022-05-24 11:06:35 -0400

    Added a field to the userserializer

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -6,7 +6,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields =['first_name', 'last_name','email','role']
+        fields =['id','first_name', 'last_name','email','role']
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
