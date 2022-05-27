0287e8902fb36ece0e2f00e2e3f771a3b1b0f569
Reny Reyes <renyreyes8@hotmail.com>
2022-05-27 14:27:05 -0400

    Added css to the print button

--- frontend/src/PDFfeature/ExportPDF.jsx
+++ frontend/src/PDFfeature/ExportPDF.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import ReactToPrint from 'react-to-print';
-import PDFlayout from "./PDFlayout"
- 
-class ExportPDF extends React.Component{
-     
-    render() {
-      return (
-        <div className="export">
+import React from "react";
+import ReactToPrint from "react-to-print";
+import PDFlayout from "./PDFlayout";
+
+class ExportPDF extends React.Component {
+  render() {
+    return (
+      <div className="export">
         <div className="box">
+          <div className="around-button">
           <ReactToPrint
             content={() => this.componentRef}
             trigger={() => <button className="button">Print PDF</button>}
-          />
-          <PDFlayout projects={this.props.projects} tasks={this.props.tasks}ref={(response) => (this.componentRef = response)} />
-          
-        </div></div>
-      );
-    }
+          /></div>
+        <PDFlayout
+          projects={this.props.projects}
+          tasks={this.props.tasks}
+          ref={(response) => (this.componentRef = response)}
+        /></div>
+      </div>
+    );
+  }
 }
- 
+
 export default ExportPDF;
