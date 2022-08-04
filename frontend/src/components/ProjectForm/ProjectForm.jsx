91f0461f4b2dc1f283497d54428173ad2b89cfc8
Reny Reyes <renyreyes8@hotmail.com>
2022-08-04 09:12:30 -0400

    Added bootstrap accordion for the project form

--- frontend/src/components/ProjectForm/ProjectForm.jsx
+++ frontend/src/components/ProjectForm/ProjectForm.jsx
@@ -1,10 +1,20 @@
 import axios from "axios";
-import React, { useState} from "react";
+import React, { useState } from "react";
 
 const ProjectForm = (props) => {
   const [title, setTitle] = useState("");
   const [dueDate, setDueDate] = useState("");
+  const [collapse, setCollapse] = useState("accordion-collapse collapse")
 
+  function toggle() {
+    if (collapse === "accordion-collapse collapse") {
+      setCollapse("accordion-collapse collapse show")
+    }
+    else {
+      setCollapse("accordion-collapse collapse")
+    }
+    
+  }
   function createProject(event) {
     event.preventDefault();
     let newProject = {
@@ -13,10 +23,9 @@ const ProjectForm = (props) => {
       due_date: dueDate,
     };
     addProject(newProject);
-    setTitle("")
-    setDueDate("")
+    setTitle("");
+    setDueDate("");
   }
-
 
   async function addProject(addNew) {
     try {
@@ -31,35 +40,57 @@ const ProjectForm = (props) => {
     }
   }
   return (
-    <div>
-      <form className="assigned-form-container" onSubmit={createProject}>
-        <div className="border-box">
-          <h2>Create Project</h2>
-          <div className="grid-box">
-            <label>
-              DUE DATE:
-              <input
-                type="date"
-                value={dueDate}
-                onChange={(e) => setDueDate(e.target.value)}
-              />
-            </label>
+    <div className="accordion" id="projectAccordion">
+      <div className="accordion-item">
+        <form className="assigned-form-container" onSubmit={createProject}>
+          <div>
+            <h2 className="accordion-header" id="headingOne">
+              <button
+              onClick={()=> toggle()}
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >Create Project</button>
+              
+            </h2>
+            <div
+              id="collapseOne"
+              className={collapse}
+              aria-labelledby="headingOne"
+              data-bs-parent="#projectAccordion"
+            >
+              <div className="accordion-body">
+                <div className="grid-box">
+                  <label>
+                    DUE DATE:
+                    <input
+                      type="date"
+                      value={dueDate}
+                      onChange={(e) => setDueDate(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <div className="grid-box">
+                  <label>
+                    TITLE:
+                    <input
+                      type="text"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <div className="grid-box">
+                  <button className="button">SUBMIT</button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="grid-box">
-            <label>
-              TITLE:
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </label>
-          </div>
-          <div className="grid-box">
-            <button className="button">SUBMIT</button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
