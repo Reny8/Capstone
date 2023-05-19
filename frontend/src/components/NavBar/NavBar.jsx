27b3445cf5c573d35247f30cf87a291b1c8bb5fd
Reny Reyes <renyreyes8@hotmail.com>
2023-05-18 20:10:18 -0400

    Refactor navbar code

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,93 +1,72 @@
-import React from "react";
-import { useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
-import "./NavBar.css";
-import Logo from "../../output-onlinegiftools.gif";
+import React from 'react';
+import { useContext } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import AuthContext from '../../context/AuthContext';
+import './NavBar.css';
+import Logo from '../../output-onlinegiftools.gif';
 const Navbar = (props) => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
-  if (props.user && props.user.role === "Project Manager") {
     return (
       <div>
-        <ul className="nav-bar">
-          <li className="brand">
-            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-              <h2 style={{ color: "white" }}>
-                <img src={Logo} alt="Comany Logo" style={{ height: "3rem" }} />
-                Project Tracker
-              </h2>
+        <ul className='nav-bar'>
+          <li className='brand'>
+            <Link
+              to='/'
+              style={{ textDecoration: 'none' }}
+              title='Project Tracker Home'
+            >
+              <img src={Logo} alt='Comany Logo' style={{ height: '3rem' }} />
             </Link>
           </li>
           <li>
-            <button onClick={() => navigate("/")} className="button">
-              <img src="https://img.icons8.com/fluency-systems-regular/48/6187C2/home.png" alt="Home"/>{" "}
+            <button onClick={() => navigate('/')} className='button'>
+              <img
+                src='https://img.icons8.com/fluency-systems-regular/48/6187C2/home.png'
+                alt='Home'
+              />{' '}
             </button>
           </li>
           <li>
-            <button onClick={() => navigate("/logs")} className="button">
-              <img src="https://img.icons8.com/pastel-glyph/48/6187C2/spiral-bound-booklet.png" alt="Logs"/>{" "}
+            <button onClick={() => navigate('/logs')} className='button'>
+              <img
+                src='https://img.icons8.com/pastel-glyph/48/6187C2/spiral-bound-booklet.png'
+                alt='Logs'
+              />{' '}
             </button>
           </li>
           <li>
-            <button onClick={() => navigate("/agenda")} className="button">
-              <img src="https://img.icons8.com/ios/48/6187C2/calendar--v1.png" alt="agenda"/>{" "}
+            <button onClick={() => navigate('/agenda')} className='button'>
+              <img
+                src='https://img.icons8.com/ios/48/6187C2/calendar--v1.png'
+                alt='agenda'
+              />{' '}
             </button>
           </li>
-          <li>
-            <button onClick={() => navigate("/print")} className="button">
-              <img src="https://img.icons8.com/ios/48/6187C2/print--v1.png" alt="Print"/>{" "}
-            </button>
-          </li>
+          {props.user && props.user.role === 'Project Manager' ? (
+            <li>
+              <button onClick={() => navigate('/print')} className='button'>
+                <img
+                  src='https://img.icons8.com/ios/48/6187C2/print--v1.png'
+                  alt='Print'
+                />{' '}
+              </button>
+            </li>
+          ) : null}
           <li>
             {user ? (
-              <button className="button" onClick={logoutUser}>
-                <img src="https://img.icons8.com/ios-glyphs/48/6187C2/logout-rounded-down.png" alt="Logout"/>{" "}
+              <button className='button' onClick={logoutUser}>
+                <img
+                  src='https://img.icons8.com/ios-glyphs/48/6187C2/logout-rounded-down.png'
+                  alt='Logout'
+                />{' '}
               </button>
             ) : (
-              <button className="button" onClick={() => navigate("/login")}>
-                <img src="https://img.icons8.com/ios-filled/48/6187C2/login-rounded-down.png" alt="Login"/>{" "}
-              </button>
-            )}
-          </li>
-        </ul>
-      </div>
-    );
-  } else
-    return (
-      <div>
-        <ul className="nav-bar">
-          <li className="brand">
-            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-              <h2 style={{ color: "white" }}>
-                <img src={Logo} alt="Comany Logo" style={{ height: "3rem" }} />
-                Project Tracker
-              </h2>
-            </Link>
-          </li>
-          <li>
-            <button onClick={() => navigate("/")} className="button">
-              <img src="https://img.icons8.com/fluency-systems-regular/48/6187C2/home.png" alt="Home"/>
-            </button>
-          </li>
-          <li>
-            <button onClick={() => navigate("/logs")} className="button">
-              <img src="https://img.icons8.com/pastel-glyph/48/6187C2/spiral-bound-booklet.png" alt="Logs"/>{" "}
-            </button>
-          </li>
-          <li>
-            <button onClick={() => navigate("/agenda")} className="button">
-              <img src="https://img.icons8.com/ios/48/6187C2/calendar--v1.png" alt="Agenda"/>{" "}
-            </button>
-          </li>
-          <li>
-            {user ? (
-              <button className="button" onClick={logoutUser}>
-                <img src="https://img.icons8.com/ios-glyphs/48/6187C2/logout-rounded-down.png" alt="Logout"/>{" "}
-              </button>
-            ) : (
-              <button className="button" onClick={() => navigate("/login")}>
-                <img src="https://img.icons8.com/ios-filled/48/6187C2/login-rounded-down.png" alt="Login"/>{" "}
+              <button className='button' onClick={() => navigate('/login')}>
+                <img
+                  src='https://img.icons8.com/ios-filled/48/6187C2/login-rounded-down.png'
+                  alt='Login'
+                />{' '}
               </button>
             )}
           </li>
